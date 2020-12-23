fc5d4af1dff3dc0a943c62194432bd302f4eb1ba
gustavo867 <tartatavo3@gmail.com>
2020-12-23 03:44:12 -0300

    :wrench: fix

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import Header from './src/components/Header';
-
 import Routes from './src/routes';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Routes />
     </>
   );
